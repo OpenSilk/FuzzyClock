23027f2773631ba668be2265a6ed4071dbc7dded
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-11-09 18:16:30 -0600

    Stop using the service to update widget

--- FuzzyClock/src/main/java/org/opensilk/fuzzyclock/FuzzyWidget.java
+++ FuzzyClock/src/main/java/org/opensilk/fuzzyclock/FuzzyWidget.java
@@ -4,9 +4,11 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import java.util.Calendar;
 
@@ -15,13 +17,13 @@ public class FuzzyWidget extends AppWidgetProvider {
     private static final String TAG = FuzzyWidget.class.getSimpleName();
     private static final boolean LOGV = true;
 
-    private static final String ACTION_POKE_SERVICE = "org.opensilk.action.POKE_SERVICE";
+    private static final String ACTION_UPDATE_WIDGET = "org.opensilk.action.UPDATE_FUZZY_WIDGET";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (ACTION_POKE_SERVICE.equals(action)) {
-            pokeService(context);
+        if (ACTION_UPDATE_WIDGET.equals(action)) {
+            updateTime(context);
         } else {
             super.onReceive(context, intent);
         }
@@ -30,26 +32,19 @@ public class FuzzyWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         if (LOGV) Log.v(TAG, "onUpdate");
-        pokeService(context);
+        updateTime(context);
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         if (LOGV) Log.v(TAG, "onDeleted");
-        pokeService(context);
+        updateTime(context);
     }
 
     @Override
     public void onDisabled(Context context) {
         if (LOGV) Log.v(TAG, "onDisabled");
-        context.stopService(new Intent(context, FuzzyWidgetService.class));
         cancelNextAlarm(context);
-    }
-
-    private void pokeService(Context context) {
-        if (LOGV) Log.v(TAG, "pokeService");
-        context.startService(new Intent(context, FuzzyWidgetService.class));
-        setNextAlarm(context);
     }
 
     void setNextAlarm(Context context) {
@@ -75,7 +70,7 @@ public class FuzzyWidget extends AppWidgetProvider {
         nextMilli += (nextHours - hours) * 60 * 60 * 1000;
         nextMilli += (nextMinutes - minutes) * 60 * 1000;
 
-        Intent nextUpdate = new Intent(ACTION_POKE_SERVICE);
+        Intent nextUpdate = new Intent(ACTION_UPDATE_WIDGET);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, nextUpdate, PendingIntent.FLAG_CANCEL_CURRENT);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         am.cancel(pi);
@@ -84,9 +79,152 @@ public class FuzzyWidget extends AppWidgetProvider {
     }
 
     void cancelNextAlarm(Context context) {
-        Intent nextUpdate = new Intent(ACTION_POKE_SERVICE);
+        Intent nextUpdate = new Intent(ACTION_UPDATE_WIDGET);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, nextUpdate, PendingIntent.FLAG_CANCEL_CURRENT);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         am.cancel(pi);
+    }
+
+    private void updateTime(Context context) {
+        Calendar calendar = Calendar.getInstance();
+        boolean is24HourFormat = android.text.format.DateFormat.is24HourFormat(context);
+
+//        calendar.setTimeInMillis(System.currentTimeMillis());
+//        if (mTimeZoneId != null) {
+//            calendar.setTimeZone(TimeZone.getTimeZone(mTimeZoneId));
+//        }
+
+        int minutes, hours;
+        CharSequence timeH, timeM, separator;
+
+        minutes = calendar.get(Calendar.MINUTE);
+
+        if        (minutes >= 56) { timeM = ""; // O'CLOCK
+        } else if (minutes >= 51) { timeM = context.getString(R.string.fuzzy_five);
+        } else if (minutes >= 46) { timeM = context.getString(R.string.fuzzy_ten);
+        } else if (minutes >= 41) { timeM = context.getString(R.string.fuzzy_quarter);
+        } else if (minutes >= 36) { timeM = context.getString(R.string.fuzzy_twenty);
+        } else if (minutes >= 25) { timeM = context.getString(R.string.fuzzy_half);
+        } else if (minutes >= 20) { timeM = context.getString(R.string.fuzzy_twenty);
+        } else if (minutes >= 15) { timeM = context.getString(R.string.fuzzy_quarter);
+        } else if (minutes >= 10) { timeM = context.getString(R.string.fuzzy_ten);
+        } else if (minutes >= 5)  { timeM = context.getString(R.string.fuzzy_five);
+        } else                    { timeM = ""; // O'CLOCK
+        }
+
+        hours = calendar.get(is24HourFormat ? Calendar.HOUR_OF_DAY : Calendar.HOUR);
+
+        // Adjust for next hour
+        if (minutes >= 36) {
+            if (is24HourFormat) {
+                hours = (hours + 1) % 24;
+            } else {
+                hours = (hours + 1) % 12;
+            }
+        }
+
+        switch (hours) {
+            case 0:  timeH = context.getString(R.string.fuzzy_twelve); break;
+            case 1:  timeH = context.getString(R.string.fuzzy_one); break;
+            case 2:  timeH = context.getString(R.string.fuzzy_two); break;
+            case 3:  timeH = context.getString(R.string.fuzzy_three); break;
+            case 4:  timeH = context.getString(R.string.fuzzy_four); break;
+            case 5:  timeH = context.getString(R.string.fuzzy_five); break;
+            case 6:  timeH = context.getString(R.string.fuzzy_six); break;
+            case 7:  timeH = context.getString(R.string.fuzzy_seven); break;
+            case 8:  timeH = context.getString(R.string.fuzzy_eight); break;
+            case 9:  timeH = context.getString(R.string.fuzzy_nine); break;
+            case 10: timeH = context.getString(R.string.fuzzy_ten); break;
+            case 11: timeH = context.getString(R.string.fuzzy_eleven); break;
+            case 12: timeH = context.getString(R.string.fuzzy_twelve); break;
+            case 13: timeH = context.getString(R.string.fuzzy_thirteen); break;
+            case 14: timeH = context.getString(R.string.fuzzy_fourteen); break;
+            case 15: timeH = context.getString(R.string.fuzzy_fifteen); break;
+            case 16: timeH = context.getString(R.string.fuzzy_sixteen); break;
+            case 17: timeH = context.getString(R.string.fuzzy_seventeen); break;
+            case 18: timeH = context.getString(R.string.fuzzy_eighteen); break;
+            case 19: timeH = context.getString(R.string.fuzzy_nineteen); break;
+            case 20: timeH = context.getString(R.string.fuzzy_twenty); break;
+            case 21: timeH = context.getString(R.string.fuzzy_twenty) + context.getString(R.string.fuzzy_one); break;
+            case 22: timeH = context.getString(R.string.fuzzy_twenty) + context.getString(R.string.fuzzy_two); break;
+            case 23: timeH = context.getString(R.string.fuzzy_twenty) + context.getString(R.string.fuzzy_three); break;
+            case 24: timeH = context.getString(R.string.fuzzy_twenty) + context.getString(R.string.fuzzy_four); break;
+            default: timeH = ""; break;
+        }
+
+        // Handle Noon and Midnight
+        if (is24HourFormat) {
+            if (hours == 12) {
+                timeH = context.getString(R.string.fuzzy_noon);
+            } else if (hours == 0) {
+                timeH = context.getString(R.string.fuzzy_midnight);
+            }
+        } else {
+            if (hours == 0) {
+                if (minutes >= 36) {
+                    timeH = (calendar.get(Calendar.AM_PM) == 1) ?
+                            context.getString(R.string.fuzzy_midnight) :
+                            context.getString(R.string.fuzzy_noon);
+                } else {
+                    timeH = (calendar.get(Calendar.AM_PM) == 0) ?
+                            context.getString(R.string.fuzzy_midnight) :
+                            context.getString(R.string.fuzzy_noon);
+                }
+            }
+        }
+
+        // Final shuffle before writing to display
+        if (minutes >= 56) {
+            separator = (hours > 12) ?
+                    context.getString(R.string.fuzzy_hundred) :
+                    context.getString(R.string.fuzzy_oclock);
+            if (hours == 0 || (is24HourFormat && (hours == 12))) {
+//                separator = (mCalendar.get(Calendar.AM_PM) == 1) ?
+//                        context.getString(R.string.fuzzy_midnight) :
+//                        context.getString(R.string.fuzzy_noon);
+                separator = timeH;
+                timeM = "";
+            } else {
+                timeM = timeH;
+            }
+            timeH = "";
+        } else if (minutes >= 36) {
+            separator = context.getString(R.string.fuzzy_to);
+        } else if (minutes >= 5) {
+            separator = context.getString(R.string.fuzzy_past);
+        } else {
+            separator = (hours > 12) ?
+                    context.getString(R.string.fuzzy_hundred) :
+                    context.getString(R.string.fuzzy_oclock);
+            if (hours == 0 || (is24HourFormat && (hours == 12))) {
+//                separator = (mCalendar.get(Calendar.AM_PM) == 0) ?
+//                        context.getString(R.string.fuzzy_midnight) :
+//                        context.getString(R.string.fuzzy_noon);
+                separator = timeH;
+                timeM = "";
+            } else {
+                timeM = timeH;
+            }
+            timeH = "";
+        }
+
+        // Write time to the display
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.fuzzy_widget);
+
+        views.setTextViewText(R.id.timeDisplayMinutes, timeM);
+        views.setTextViewText(R.id.timeDisplaySeparator, separator);
+        views.setTextViewText(R.id.timeDisplayHours, timeH);
+
+        AppWidgetManager appManager = AppWidgetManager.getInstance(context);
+
+        int[] widgetIds = appManager.getAppWidgetIds(new ComponentName(context, FuzzyWidget.class));
+        if (widgetIds != null && widgetIds.length > 0) {
+            if (LOGV) for (int id: widgetIds) Log.v(TAG, "Updating widget id=" + id);
+            appManager.updateAppWidget(widgetIds, views);
+            setNextAlarm(context);
+        } else {
+            Log.i(TAG, "No widgets left to update...");
+            cancelNextAlarm(context);
+        }
     }
 }

e0fce3b96932c343afcf80eceb3f7d6780699693
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-04-09 20:32:00 -0500

    spruce up the color/typeface prefs
    Change-Id: I0ffcb66e430ca684033ccb347ea01c22a0aa24e6

--- FuzzyClock/src/main/java/org/opensilk/fuzzyclock/FuzzySettingsPrefsPage.java
+++ FuzzyClock/src/main/java/org/opensilk/fuzzyclock/FuzzySettingsPrefsPage.java
@@ -19,14 +19,18 @@ package org.opensilk.fuzzyclock;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import hugo.weaving.DebugLog;
 
@@ -44,49 +48,17 @@ public class FuzzySettingsPrefsPage extends Fragment implements
     private Button mColorButton;
     private Button mFontStyle;
 
-    private CharSequence[] mColorEntries;
-    private int[] mColorResources;
-
-    private CharSequence[] mFontStyleEntries;
-
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        Bundle b = getArguments();
-        mPref = b.getString("pref");
         mActivity = (FuzzySettings) activity;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mColorResources = new int[] {
-                getResources().getColor(android.R.color.holo_blue_light),
-                getResources().getColor(android.R.color.holo_red_light),
-                getResources().getColor(android.R.color.holo_green_light),
-                getResources().getColor(android.R.color.holo_orange_dark),
-                getResources().getColor(android.R.color.holo_purple),
-                getResources().getColor(android.R.color.white),
-                0
-        };
-        mColorEntries = new CharSequence[] {
-                getString(R.string.holo_blue),
-                getString(R.string.holo_red),
-                getString(R.string.holo_green),
-                getString(R.string.holo_orange),
-                getString(R.string.holo_purple),
-                getString(R.string.white),
-                getString(R.string.custom)
-        };
-        // DO NOT REORDER
-        mFontStyleEntries = new CharSequence[] {
-                getString(R.string.regular),
-                getString(R.string.regular_bold),
-                getString(R.string.condensed),
-                getString(R.string.condensed_bold),
-                getString(R.string.thin),
-                getString(R.string.this_bold)
-        };
+        Bundle b = getArguments();
+        mPref = b.getString("pref");
     }
 
     @Override
@@ -134,7 +106,7 @@ public class FuzzySettingsPrefsPage extends Fragment implements
         if (v == mColorButton) {
             chooseColor();
         } else if (v == mFontStyle) {
-            chooseFontStyle();
+            chooseTypeface();
         }
     }
 
@@ -155,103 +127,220 @@ public class FuzzySettingsPrefsPage extends Fragment implements
         mActivity.notifyPrefChanged();
     }
 
-    protected int getColorEntry() {
-        int color = 0;
+    /**
+     * Checks if given color is current color, used to set radio buttons
+     * @param color
+     * @return
+     */
+    protected boolean checkColor(int color) {
         switch (mPref) {
             case "hour":
-                color = mActivity.mFuzzyPrefs.hour.color;
-                break;
+                return mActivity.mFuzzyPrefs.hour.color == color;
             case "separator":
-                color = mActivity.mFuzzyPrefs.separator.color;
-                break;
+                return mActivity.mFuzzyPrefs.separator.color == color;
             case "minute":
-                color = mActivity.mFuzzyPrefs.minute.color;
-                break;
+                return mActivity.mFuzzyPrefs.minute.color == color;
         }
-        for (int ii=0;ii< mColorResources.length;ii++) {
-            if (mColorResources[ii] == color) {
-                return ii;
-            }
-        }
-        return mColorResources.length-1;
+        return false;
     }
 
-    protected void setNewColor(int which) {
+    /**
+     * Applies new color and notifies activity to update preview
+     * @param color
+     */
+    protected void setNewColor(int color) {
         switch (mPref) {
             case "hour":
-                mActivity.mFuzzyPrefs.hour.color = mColorResources[which];
+                mActivity.mFuzzyPrefs.hour.color = color;
                 break;
             case "separator":
-                mActivity.mFuzzyPrefs.separator.color = mColorResources[which];
+                mActivity.mFuzzyPrefs.separator.color = color;
                 break;
             case "minute":
-                mActivity.mFuzzyPrefs.minute.color = mColorResources[which];
+                mActivity.mFuzzyPrefs.minute.color = color;
                 break;
         }
         mActivity.notifyPrefChanged();
     }
 
+    /**
+     * Creates the color preference dialog
+     */
     @DebugLog
     protected void chooseColor() {
+        final ColorPrefAdapter.ColorChoice[] colorChoices = new ColorPrefAdapter.ColorChoice[] {
+                new ColorPrefAdapter.ColorChoice(getString(R.string.holo_blue),
+                        getResources().getColor(android.R.color.holo_blue_light)),
+                new ColorPrefAdapter.ColorChoice(getString(R.string.holo_red),
+                        getResources().getColor(android.R.color.holo_red_light)),
+                new ColorPrefAdapter.ColorChoice(getString(R.string.holo_green),
+                        getResources().getColor(android.R.color.holo_green_light)),
+                new ColorPrefAdapter.ColorChoice(getString(R.string.holo_orange),
+                        getResources().getColor(android.R.color.holo_orange_dark)),
+                new ColorPrefAdapter.ColorChoice(getString(R.string.holo_purple),
+                        getResources().getColor(android.R.color.holo_purple)),
+                new ColorPrefAdapter.ColorChoice(getString(R.string.white),
+                        getResources().getColor(android.R.color.white)),
+                new ColorPrefAdapter.ColorChoice(getString(R.string.custom),
+                        // Color is just to set text color, not an available choice
+                        getResources().getColor(android.R.color.tertiary_text_dark))
+        };
+        for (ColorPrefAdapter.ColorChoice c : colorChoices) {
+            c.isActive = checkColor(c.value);
+        }
+        final ColorPrefAdapter adapter = new ColorPrefAdapter(getActivity(), 0, colorChoices);
         new AlertDialog.Builder(mActivity)
-                .setSingleChoiceItems(mColorEntries,
-                        getColorEntry(),
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                if (which == mColorEntries.length-1) {
-                                    FuzzyColorPickerDialog.newInstance(mPref).show(getFragmentManager(), "color_picker");
-                                } else {
-                                    setNewColor(which);
-                                }
-                            }
-                        })
+                .setAdapter(adapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        if (which == colorChoices.length - 1) {
+                            FuzzyColorPickerDialog.newInstance(mPref).show(getFragmentManager(), "color_picker");
+                        } else {
+                            setNewColor(adapter.getItem(which).value);
+                        }
+                    }
+                })
                 .show();
     }
 
-    protected int getFontEntry() {
-        int style = -1;
+    /**
+     * Checks if given style is current font style, used to set radio buttons
+     * @param style
+     * @return
+     */
+    protected boolean checkTypeface(int style) {
         switch (mPref) {
             case "hour":
-                style = mActivity.mFuzzyPrefs.hour.style;
-                break;
+                return mActivity.mFuzzyPrefs.hour.style == style;
             case "separator":
-                style = mActivity.mFuzzyPrefs.separator.style;
-                break;
+                return mActivity.mFuzzyPrefs.separator.style == style;
             case "minute":
-                style = mActivity.mFuzzyPrefs.minute.style;
-                break;
+                return mActivity.mFuzzyPrefs.minute.style == style;
         }
-        return style;
+        return false;
     }
 
-    protected void setNewFontStyle(int which) {
+    /**
+     * Applies the new font style and notifies activity to update preview
+     * @param style
+     */
+    protected void setNewTypeface(int style) {
         switch (mPref) {
             case "hour":
-                mActivity.mFuzzyPrefs.hour.style = which;
+                mActivity.mFuzzyPrefs.hour.style = style;
                 break;
             case "separator":
-                mActivity.mFuzzyPrefs.separator.style = which;
+                mActivity.mFuzzyPrefs.separator.style = style;
                 break;
             case "minute":
-                mActivity.mFuzzyPrefs.minute.style = which;
+                mActivity.mFuzzyPrefs.minute.style = style;
                 break;
         }
         mActivity.notifyPrefChanged();
     }
 
-    protected void chooseFontStyle() {
+    /**
+     * Creates the typeface preference dialog
+     */
+    protected void chooseTypeface() {
+        // DO NOT REORDER
+        final TypefacePrefAdapter.TypefaceChoice[] typefaceChoices = new TypefacePrefAdapter.TypefaceChoice[] {
+                new TypefacePrefAdapter.TypefaceChoice(getString(R.string.regular)),
+                new TypefacePrefAdapter.TypefaceChoice(getString(R.string.regular_bold)),
+                new TypefacePrefAdapter.TypefaceChoice(getString(R.string.condensed)),
+                new TypefacePrefAdapter.TypefaceChoice(getString(R.string.condensed_bold)),
+                new TypefacePrefAdapter.TypefaceChoice(getString(R.string.thin)),
+                new TypefacePrefAdapter.TypefaceChoice(getString(R.string.thin_bold))
+        };
+        for (int ii=0; ii< typefaceChoices.length; ii++) {
+            typefaceChoices[ii].isActive = checkTypeface(ii);
+        }
+        final TypefacePrefAdapter adapter = new TypefacePrefAdapter(getActivity(), 0, typefaceChoices);
         new AlertDialog.Builder(mActivity)
-                .setSingleChoiceItems(mFontStyleEntries,
-                        getFontEntry(),
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                setNewFontStyle(which);
-                                dialog.dismiss();
-                            }
-                        })
+                .setAdapter(adapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setNewTypeface(which);
+                        dialog.dismiss();
+                    }
+                })
                 .show();
+    }
+
+    /**
+     * Adapter for font color dialog
+     */
+    protected static class ColorPrefAdapter extends ArrayAdapter<ColorPrefAdapter.ColorChoice> {
+
+        protected static class ColorChoice {
+            protected CharSequence name;
+            protected int value;
+            protected boolean isActive;
+            protected ColorChoice(CharSequence name, int value) {
+                this.name=name;
+                this.value=value;
+            }
+        }
+
+        public ColorPrefAdapter(Context context, int resource, ColorChoice[] objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v;
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = inflater.inflate(R.layout.fuzzy_settings_pref_fancy_list_item, parent, false);
+            } else {
+                v = convertView;
+            }
+            if (v != null) {
+                TextView tv = (TextView) v.findViewById(R.id.entry_name);
+                tv.setText(getItem(position).name);
+                tv.setTextColor(getItem(position).value);
+                RadioButton r = (RadioButton) v.findViewById(R.id.radio_btn);
+                r.setChecked(getItem(position).isActive);
+            }
+            return v;
+        }
+    }
+
+    /**
+     * Adapter for font style dialog
+     */
+    protected static class TypefacePrefAdapter extends ArrayAdapter<TypefacePrefAdapter.TypefaceChoice> {
+
+        protected static class TypefaceChoice {
+            protected CharSequence name;
+            protected boolean isActive;
+            protected TypefaceChoice(CharSequence name) {
+                this.name=name;
+            }
+        }
+
+        public TypefacePrefAdapter(Context context, int resource, TypefaceChoice[] objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v;
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = inflater.inflate(R.layout.fuzzy_settings_pref_fancy_list_item, parent, false);
+            } else {
+                v = convertView;
+            }
+            if (v != null) {
+                TextView tv = (TextView) v.findViewById(R.id.entry_name);
+                tv.setText(getItem(position).name);
+                tv.setTypeface(FuzzyPrefs.createTypeface(position));
+                RadioButton r = (RadioButton) v.findViewById(R.id.radio_btn);
+                r.setChecked(getItem(position).isActive);
+            }
+            return v;
+        }
     }
 }
